ce42685fa774aa70cd3bb6af96495fceef1deef6
J3r3mC <j.corbeilla@outlook.fr>
2025-05-19 15:46:43 +0200

    ADD architecture

---
+++ auth-service/routes/auth.js
@@ -0,0 +1,9 @@
+// auth-service/routes/auth.js
+const express = require('express');
+const router = express.Router();
+const { register, login } = require('../controllers/authController');
+
+router.post('/register', register);
+router.post('/login', login);
+router.get('/logout', logout);
+module.exports = router;
