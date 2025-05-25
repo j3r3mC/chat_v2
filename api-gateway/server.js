53586f6e8a20512288cdf0f727af5e90d8f289d5
J3r3mC <j.corbeilla@outlook.fr>
2025-05-25 02:43:35 +0200

    FIX port

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -27,5 +27,5 @@ app.use('/api/admin', createProxyMiddleware({
 
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => console.log(`API Gateway running on port ${PORT}`));
