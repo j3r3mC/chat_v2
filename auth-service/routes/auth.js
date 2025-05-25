72c920d200a1da3abfeb18739c5a0f694b370ca3
J3r3mC <j.corbeilla@outlook.fr>
2025-05-25 23:27:01 +0200

    FIX Register redirection Register OK

--- auth-service/routes/auth.js
+++ auth-service/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { register, registerAdmin, login, logout } = require('../controllers/auth-controller');
 
-router.post('/register', register); // Inscription standard
+router.post('/register', register); // Inscription standard et superadmin
 router.post('/register-admin', registerAdmin); // Inscription admin via superadmin
 router.post('/login', login);
 router.get('/logout', logout);
